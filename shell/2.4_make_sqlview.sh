6cd303e7df6cf82bc36c772cca516c2f768a552a
cyt <178462289@qq.com>
2019-06-17 16:29:24 +0800

    调整pg执行顺序

--- shell/2.4_make_sqlview.sh
+++ shell/2.4_make_sqlview.sh
@@ -2,7 +2,6 @@
 
 # ===============postgrseql数据导入==============
 su postgres << EOF
-createdb mote_basemap;
 psql -d mote_basemap -U postgres -f /mnt/shell/pgr_fromatob.sql;
 psql -d mote_basemap -U postgres -f /mnt/shell/pgr_fromatob_withfloor.sql;
 psql -d mote_basemap -U postgres -f /mnt/shell/pgr_pointinpolygon.sql;
