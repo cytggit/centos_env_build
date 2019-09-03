996069582e0c18cd2f44b4c315999b3c492e2b74
cyt <178462289@qq.com>
2019-09-03 14:33:23 +0800

    环境变量

--- shell/2.3_make_pgdata.sh
+++ shell/2.3_make_pgdata.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # ===============postgrseql数据导入==============
+source /etc/profile
 su postgres << EOF
 createdb mote_basemap;
 psql -d mote_basemap -U postgres -f /mnt/shell/insertpgdata.sql;
